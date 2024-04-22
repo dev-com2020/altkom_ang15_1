7ba537f001f3681a6a397f8bd2d9cacff45fdd61
DEV-COM <kwp@tlen.pl>
2024-04-22 15:56:44 +0200

    final 22.04

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { ProductDetailComponent } from '../product-detail/product-detail.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements AfterViewInit {
 selectedProduct = 'nieznany'
+
+@ViewChild(ProductDetailComponent) productDetail: ProductDetailComponent | undefined
+
+ngAfterViewInit(): void {
+    if (this.productDetail){
+      console.log(this.productDetail.name)
+    }
+}
 
 onBuy(name: string) {
   window.alert(`Dodaleś do listy ${name}!`)
