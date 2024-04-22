2aae22daccb9d87c4e2b2161e7a62027b8e34d1a
DEV-COM <kwp@tlen.pl>
2024-04-22 14:14:43 +0200

    14:13 22.04

---
+++ src/app/products/products.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent
+  ],
+  imports: [
+    CommonModule,
+    ProductComponent
+  ]
+})
+export class ProductsModule { }
