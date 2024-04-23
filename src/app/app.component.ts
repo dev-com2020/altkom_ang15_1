b7b112a0b89186c8f3bfd0cd67befee10b47c8e2
DEV-COM <kwp@tlen.pl>
2024-04-23 15:56:36 +0200

    zmiana title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { Component, OnDestroy } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnDestroy {
-  title = 'moja apka v. 0.1';
-  opis = 'pierwszy dzien szkolenia';
+  title = 'moja apka v. 0.3';
+  opis = 'drugi dzien szkolenia';
   ngOnDestroy(): void {
     console.log('dzialam w OnDestroy')
   }
