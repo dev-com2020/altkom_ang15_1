70af83d0047006cdc1ddfaaed149db9e16e9461f
Jabi <michal.jabi@gmail.com>
2024-04-24 12:32:17 +0200

    fix(favorites.component): DI fix + remove selector to show that we want to handle it by routing.

--- src/app/products/favorites/favorites.component.ts
+++ src/app/products/favorites/favorites.component.ts
@@ -3,14 +3,15 @@ import { Product } from '../product';
 import { ProductsService } from '../products.service';
 
 @Component({
-  selector: 'app-favorites',
+  // selector: 'app-favorites', celowo zabieram selektor (bo chcę żeby to bylo przez router obslugiwane)
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent implements OnInit {
   products: Product[] = []
 
-  constructor(@Host() @Optional() private productService: ProductsService) {}
+  //constructor(@Host() @Optional() private productService: ProductsService) {}
+  constructor(private productService: ProductsService) {}
 
   private getProducts(){
     this.productService.getProducts().subscribe(products => {
